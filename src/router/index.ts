d45208746658f2577f5a3c6b0afee6e4cbdb43ef
Mauro Hernandez <mhernandez@prolesys.com>
2024-06-25 10:10:57 -0400

    [add]: Endpoint for check the status of the API

--- src/router/index.ts
+++ src/router/index.ts
@@ -10,6 +10,11 @@ function loadRouter(main: Router): void {
   EmployeesRouter(router);
 
   main.use('/api', router);
+
+  // Check the status of the API
+  main.get('/status-api', (req, res) => {
+    res.status(200).send({ msg: 'API run.' });
+  })
 }
 
 export default loadRouter;
